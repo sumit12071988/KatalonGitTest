fa7daf3f8c25c5f037bdee9c720b5119cd011e60
sumitdemo-prog <sumit.saha.1988@gmail.com>
2020-05-09 09:33:23 +0800

    new commmit

---
+++ Test Suites/TSC001.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TSC001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>sumit786.88@gmail.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fb2b1e66-6d44-4efb-930b-3fe771a2517b</testSuiteGuid>
+   <testCaseLink>
+      <guid>2303704c-7770-4716-bf3e-352b66db7c6d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001_First User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>53541955-2822-4131-b26c-25e5af9b458e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC002_Second User</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
